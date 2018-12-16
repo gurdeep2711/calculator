20e115b7b5d01ae2bfea210d359fab8319b8e300
Gurdeep Singh <gurdeepsingh.devgun@gmail.com>
2018-12-16 11:50:07 +0530

    calculation featured added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,29 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NavController, ModalController } from 'ionic-angular';
 import { ITEMS, SHIFTS } from '../../models/items';
 import { LongPressComponent } from '../../components/long-press/long-press';
+import { Subject } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { from } from 'rxjs/observable/from';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
   ITEMS_LIST: {} = ITEMS;
   CURRUNT_SHIFT: string = 'morning';
   toogleShifts = false;
   SHIFTS = SHIFTS;
+  totalAmount: number;
+  finalOrder = [];
+
   private previousShit = 'morning';
+  private _subscription = new Subject();
+
   constructor(public navCtrl: NavController, private modalCtrl: ModalController) { }
+
+  ngOnInit(): void {
+    this._subscription.subscribe(() => {
+      this.totalAmount = 0;
+      this.finalOrder = [];
+      from(this.ITEMS_LIST[this.CURRUNT_SHIFT])
+        .pipe(
+          filter(item => item['qty'] > 0)
+        )
+        .subscribe(item => {
+          this.totalAmount += item['price'] * item['qty'];
+          this.finalOrder.push(item);
+        });
+    })
+  }
+
+  ngOnDestroy(): void {
+    this._subscription.unsubscribe();
+  }
 
   addNewOrder(item): void {
     item.qty++;
+    this._subscription.next();
+  }
+
+  clearOrder(item): void {
+    item.qty = 0;
+    this._subscription.next();
+  }
+
+  clearAllOrders(): void {
+    this.ITEMS_LIST[this.CURRUNT_SHIFT].forEach(item => {
+      item.qty = 0;
+    });
+    this._subscription.next();
   }
 
   longPressEvent(item): void {
     let modal = this.modalCtrl.create(LongPressComponent, { data: item });
     modal.onDidDismiss(data => {
       if (data) {
-        console.log(data);
+        this._subscription.next();
       }
     });
     modal.present();
@@ -34,45 +74,10 @@ export class HomePage {
       item.qty = 0;
     });
     this.previousShit = this.CURRUNT_SHIFT;
+    this._subscription.next();
   }
 
-  // adding a new order 
-  // addToOrder(item, qty, shift): void {
-  //   let flag = 0;
-  //   let temp = 0;
-  //   temp = parseInt(qty);
-  //   if (this.ORDERS.length > 0) {
-  //     for (let i = 0; i < this.ORDERS.length; i++) {
-  //       if (item.id === this.ORDERS[i]['id']) {
-  //         item.qty = parseInt(item.qty);
-  //         item.qty += temp;
-  //         flag = 1;
-  //         break;
-  //       }
-  //     }
-
-  //     if (flag === 0) {
-  //       item.qty = 1;
-  //     }
-  //     if (item.qty < 2) {
-  //       this.order.push(item);
-  //     }
-  //   } else {
-  //     this.shift = shift;
-  //     item.qty = qty;
-  //     this.order.push(item);
-  //   }
-  // }
-
-  // getDate(): void {
-  //   let today = new Date();
-  //   let mm = today.getMonth() + 1;
-  //   let dd = today.getDate();
-  //   let yyyy = today.getFullYear();
-
-  //   let date = dd + "/" + mm + "/" + yyyy
-
-  //   return date
-  // }
-
+  checkoutOrder(): void {
+    console.log(this.finalOrder);
+  }
 }
