849d22de26168612c47f227d4ab81ecbda33c766
Hitesh Malhotra <20.malhotra@gmail.com>
2018-12-30 15:27:38 +0530

    Offline data saving functionality
    Offline Data saving functionality added

---
+++ home.ts
@@ -0,0 +1,122 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NavController, ModalController, ToastController } from 'ionic-angular';
+import { ITEMS, SHIFTS } from '../../models/items';
+import { LongPressComponent } from '../../components/long-press/long-press';
+import { Subject } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { from } from 'rxjs/observable/from';
+import { HomeProvider } from '../../providers/home/home';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage implements OnInit, OnDestroy {
+  ITEMS_LIST: {} = ITEMS;
+  CURRUNT_SHIFT: string = 'morning';
+  toogleShifts = false;
+  SHIFTS = SHIFTS;
+  totalAmount: number;
+  finalOrder = [];
+
+  private previousShit = 'morning';
+  private _subscription = new Subject();
+
+  constructor(public navCtrl: NavController, private modalCtrl: ModalController,
+    private homeProv: HomeProvider, private toastCtrl: ToastController) { }
+
+  ngOnInit(): void {
+    this._subscription.subscribe(() => {
+      this.totalAmount = 0;
+      this.finalOrder = [];
+      from(this.ITEMS_LIST[this.CURRUNT_SHIFT])
+        .pipe(
+          filter(item => item['qty'] > 0)
+        )
+        .subscribe(item => {
+          this.totalAmount += item['price'] * item['qty'];
+          this.finalOrder.push(item);
+        });
+    })
+  }
+
+  ngOnDestroy(): void {
+    this._subscription.unsubscribe();
+  }
+
+  addNewOrder(item): void {
+    item.qty++;
+    this._subscription.next();
+  }
+
+  clearOrder(item): void {
+    item.qty = 0;
+    this._subscription.next();
+  }
+
+  clearAllOrders(): void {
+    this.restAll(this.CURRUNT_SHIFT);
+    this._subscription.next();
+  }
+
+  longPressEvent(item): void {
+    let modal = this.modalCtrl.create(LongPressComponent, { data: item });
+    modal.onDidDismiss(() => {
+      this._subscription.next();
+    });
+    modal.present();
+  }
+
+  restItem(): void {
+    this.restAll(this.previousShit);
+    this.previousShit = this.CURRUNT_SHIFT;
+    this._subscription.next();
+  }
+
+  checkoutOrder(): void {
+    //If totalamount > 0 then only order should be placed
+   if(this.totalAmount)
+   {    
+    let data = {
+      total: this.totalAmount,
+      orders: this.finalOrder,
+      timeStamp: new Date()
+    }
+
+    const toast = this.toastCtrl.create({
+      message: 'Thanks!, Payment received successfully.',
+      duration: 3000,
+      position: 'top'
+    });
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+    toast.present();
+
+    this.homeProv.saveOrders(this.CURRUNT_SHIFT, data).then(() => {
+      //Save data on server
+      
+    })
+    this.restAll(this.CURRUNT_SHIFT);
+    this._subscription.next();
+   }
+   else
+   {
+    const toast = this.toastCtrl.create({
+      message: 'Please add itmes before placing the order.',
+      duration: 3000,
+      position: 'top'
+    });
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+    toast.present();
+   }
+  }
+
+  private restAll(shift): void {
+    this.ITEMS_LIST[shift].forEach(item => {
+      item.qty = 0;
+    });
+  }
+}
---
+++ items.ts
@@ -0,0 +1,221 @@
+export const ITEMS = {
+    'morning': [
+        {
+            id: 1,
+            name: "Parle G",
+            price: 4,
+            img: 'assets/imgs/16.png',
+            qty: 0
+        },
+        {
+            id: 2,
+            name: "Monaco",
+            price: 4,
+            img: 'assets/imgs/11.png',
+            qty: 0
+        },
+        {
+            id: 3,
+            name: "Tea",
+            price: 5,
+            img: 'assets/imgs/22.png',
+            qty: 0
+        },
+        {
+            id: 4,
+            name: "Rusk",
+            price: 3,
+            img: 'assets/imgs/18.png',
+            qty: 0
+        },
+        {
+            id: 5,
+            name: "Burger",
+            price: 6,
+            img: 'assets/imgs/4.png',
+            qty: 0
+        },
+        {
+            id: 6,
+            name: "Jumpin",
+            price: 9,
+            img: 'assets/imgs/8.png',
+            qty: 0
+        },
+        {
+            id: 7,
+            name: "Chaach",
+            price: 4,
+            img: 'assets/imgs/5.png',
+            qty: 0
+        }
+    ],
+    'afternoon': [
+        {
+            id: 1,
+            name: "Parle G",
+            price: 4,
+            img: 'assets/imgs/16.png',
+            qty: 0
+        },
+        {
+            id: 2,
+            name: "Monaco",
+            price: 4,
+            img: 'assets/imgs/11.png',
+            qty: 0
+        },
+        {
+            id: 3,
+            name: "Tea",
+            price: 5,
+            img: 'assets/imgs/22.png',
+            qty: 0
+        },
+        {
+            id: 4,
+            name: "Rusk",
+            price: 3,
+            img: 'assets/imgs/18.png',
+            qty: 0
+        },
+        {
+            id: 5,
+            name: "Burger",
+            price: 6,
+            img: 'assets/imgs/4.png',
+            qty: 0
+        },
+        {
+            id: 6,
+            name: "Jumpin",
+            price: 9,
+            img: 'assets/imgs/8.png',
+            qty: 0
+        },
+        {
+            id: 7,
+            name: "Chaach",
+            price: 4,
+            img: 'assets/imgs/5.png',
+            qty: 0
+        },
+        {
+            id: 8,
+            name: "Thali",
+            price: 27,
+            img: 'assets/imgs/23.png',
+            qty: 0
+        },
+        {
+            id: 9,
+            name: "Raita",
+            price: 10,
+            img: 'assets/imgs/17.png',
+            qty: 0
+        },
+        {
+            id: 10,
+            name: "Packed Lunch",
+            price: 2,
+            img: 'assets/imgs/13.png',
+            qty: 0
+        },
+        {
+            id: 11,
+            name: "Subzi",
+            price: 4,
+            img: 'assets/imgs/21.png',
+            qty: 0
+        }
+    ],
+    'evening': [
+        {
+            id: 1,
+            name: "Parle G",
+            price: 4,
+            img: 'assets/imgs/16.png',
+            qty: 0
+        },
+        {
+            id: 2,
+            name: "Monaco",
+            price: 4,
+            img: 'assets/imgs/11.png',
+            qty: 0
+        },
+        {
+            id: 3,
+            name: "Tea",
+            price: 5,
+            img: 'assets/imgs/22.png',
+            qty: 0
+        },
+        {
+            id: 4,
+            name: "Rusk",
+            price: 3,
+            img: 'assets/imgs/18.png',
+            qty: 0
+        },
+        {
+            id: 5,
+            name: "Burger",
+            price: 6,
+            img: 'assets/imgs/4.png',
+            qty: 0
+        },
+        {
+            id: 6,
+            name: "Jumpin",
+            price: 9,
+            img: 'assets/imgs/8.png',
+            qty: 0
+        },
+        {
+            id: 7,
+            name: "Chaach",
+            price: 4,
+            img: 'assets/imgs/5.png',
+            qty: 0
+        },
+        {
+            id: 12,
+            name: "Pakora",
+            price: 6,
+            img: 'assets/imgs/14.png',
+            qty: 0
+        },
+        {
+            id: 13,
+            name: "Sandwich",
+            price: 6,
+            img: 'assets/imgs/19.png',
+            qty: 0
+        }
+    ],
+    'night': [
+        {
+            id: 14,
+            name: "Milk",
+            price: 9,
+            img: 'assets/imgs/10.png',
+            qty: 0
+        }
+    ]
+}
+
+export const SHIFTS = [
+    {
+        name: 'morning'
+    },
+    {
+        name: 'afternoon'
+    },
+    {
+        name: 'evening'
+    },
+    {
+        name: 'night'
+    }
+];
